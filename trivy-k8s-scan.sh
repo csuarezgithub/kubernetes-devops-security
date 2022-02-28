a179eb28072841e8af85fedf8ba0a0012324efa6
Cristian Suarez <crisuarezm@bancoripley.com>
2022-02-28 20:20:15 -0300

    added  credentials trivy k8s

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -2,9 +2,18 @@
 
 
 echo $imageName #getting Image name from env variable
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 991256897826.dkr.ecr.us-east-1.amazonaws.com
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm -v /root/.docker/config.json:/root/.docker/config.json  \
+    -v $WORKSPACE:/root/.cache/  \
+    aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+
+docker run --rm -v /root/.docker/config.json:/root/.docker/config.json  \
+    -v $WORKSPACE:/root/.cache/  \
+    aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
     # Trivy scan result processing
     exit_code=$?
